2144f1074e18ca6cd6ac0f2d2f60d71f5ee583ec
aleapoorv <apoorvale434@gmail.com>
2017-09-18 12:09:55 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,21 @@
+
+package demo;
+
+import java.util.Scanner;
+
+public class Demo {
+
+   
+    public static void main(String[] args) {
+      System.out.println("---MEnu---");
+System.out.println("1.Play");
+System.out.println("2.Instruction");
+System.out.println("3.Exit");
+System.out.println("\nChoose the option:");
+
+//Scanner sc=new Scanner(System.in);
+//int option=sc.nextInt();
+  
+  }
+    
+}
---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Hello
+{
+public static void main(String [] args){
+System.out.println("HELLO"); 
+System.out.println("---MEnu---");
+System.out.println("1.Play");
+System.out.println("2.Instruction");
+System.out.println("3.Exit");
+System.out.println("\nChoose the option:");
+Scanner sc=new Scanner(System.in);
+int option=sc.nextInt();
+}
+}
